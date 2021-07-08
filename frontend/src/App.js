f72b86b9789817179d97984ea8821843d11156e6
Adrik Gurganus <adrik@pdx.edu>
2021-07-07 17:12:39 -0700

    Update for workstation transfer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Navbar from './components/navbar';
 import homePage from './pages/home';
 import shapesPage from './pages/shapes';
 import heatPage from './pages/heat';
@@ -10,11 +11,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <React.Fragment>
+        <Navbar/>
         <Switch>
           <Route path="/" component={homePage} />
           <Route path="/shapes" component={shapesPage} />
           <Route path="/heat" component={heatPage} />
         </Switch>
+        </React.Fragment>
       </BrowserRouter>
     </div>
   );
