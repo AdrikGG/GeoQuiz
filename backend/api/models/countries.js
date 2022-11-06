c8ab3150541055ebce8ca57cdaf89394ba386d61
Adrik Gurganus <adrik@pdx.edu>
2022-11-05 20:48:46 -0700

    remove dead project code & modules

--- backend/api/models/countries.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const country = require('./country').schema;
-
-const countriesSchema = mongoose.Schema({
-    countries: [{ type: country }]
-});
-
-module.exports = mongoose.model('Countries', countriesSchema);
--- backend/api/models/country.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const countrySchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, Required: true },
-    image: { data: Buffer, contentType: String }
-});
-
-module.exports = mongoose.model('Country', countrySchema);
